7fcb33c26313252792a97d05a850901a0f18777c
ayaankhan <ayaankhan@localhost>
2025-10-02 09:39:29 -0400

    rev 1 local

---
+++ src/main/java/git/ice2_testgit/ICE2_TestGit.java
@@ -0,0 +1,14 @@
+
+
+package git.ice2_testgit;
+
+/**
+ *
+ * @author ayaankhan
+ */
+public class ICE2_TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("rev 1 local!");
+    }
+}
